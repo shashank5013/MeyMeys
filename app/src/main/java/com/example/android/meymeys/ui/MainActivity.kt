fa87ea422c65ccd1e086608dc8f8df151e0f8789
Shashank Mishra <mishrashashank501@gmail.com>
2022-01-03 10:35:36 +0530

    Navigation set up

---
+++ app/src/main/java/com/example/android/meymeys/ui/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.android.meymeys.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+import androidx.drawerlayout.widget.DrawerLayout.LOCK_MODE_UNLOCKED
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.meymeys.R
+import com.example.android.meymeys.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    //Binding Variable
+    private lateinit var binding: ActivityMainBinding
+
+    //Contains set of top level destinations
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //NavController
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Setting up NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        //Setting up bottom navigation view and drawer navigation view with navController
+        binding.apply {
+            bottomNavigationView.setupWithNavController(navController)
+            navView.setupWithNavController(navController)
+        }
+
+        //setting up Action Bar with app bar configuration
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.trendingFragment),
+            binding.drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        setupBottomNavView()
+    }
+
+    /** Handles back press when layout is open */
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    /** Sets up  bottom navigation view and drawer menu and hides it when unnecessary */
+    private fun setupBottomNavView() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.trendingFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.drawerLayout.setDrawerLockMode(LOCK_MODE_UNLOCKED)
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.INVISIBLE
+                    binding.drawerLayout.setDrawerLockMode(LOCK_MODE_LOCKED_CLOSED)
+                }
+            }
+        }
+    }
+
+
+    /** Determines how to handle up button . Also sets up Navigation Drawer **/
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+}
