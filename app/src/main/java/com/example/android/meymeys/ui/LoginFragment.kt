2ad6ba15af816c717342dcda2e41bfc37793d3d6
Shashank Mishra <mishrashashank501@gmail.com>
2022-05-12 14:04:20 +0530

    Added transitions

--- app/src/main/java/com/example/android/meymeys/ui/LoginFragment.kt
+++ app/src/main/java/com/example/android/meymeys/ui/LoginFragment.kt
@@ -18,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.lang.Exception
 
--- app/src/main/java/com/example/android/meymeys/ui/UserFragment.kt
+++ app/src/main/java/com/example/android/meymeys/ui/UserFragment.kt
@@ -1,9 +1,12 @@
 package com.example.android.meymeys.ui
 
+import android.app.ActivityManager
+import android.content.Context.ACTIVITY_SERVICE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -16,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
@@ -29,21 +33,57 @@ class UserFragment : Fragment() {
     //Auth Variable
     private lateinit var auth:FirebaseAuth
 
+    //Binding Variable
+    private lateinit var binding:FragmentUserBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding=FragmentUserBinding.inflate(layoutInflater,container,false)
+        binding=FragmentUserBinding.inflate(layoutInflater,container,false)
 
         //Initialising auth variable
-        auth=Firebase.auth
+        auth= FirebaseAuth.getInstance()
 
         //Data binding
         binding.user=auth.currentUser
         binding.executePendingBindings()
 
-        val url=auth.currentUser?.photoUrl
+        setProfilePhoto()
+
+
+        //Sign out onclick listener
+        binding.signoutBtn.setOnClickListener {
+            signOut()
+        }
+
+        return binding.root
+    }
+
+    /** Signs out user */
+    private fun signOut() {
+        auth.signOut()
+        GoogleSignIn.getClient(
+            requireContext(),
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        )
+            .signOut().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(UserFragmentDirections.actionUserFragmentToLoginFragment())
+                } else {
+                    Snackbar.make(
+                        requireView(),
+                        getString(R.string.error_text),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    /** Sets google account photo */
+    private fun setProfilePhoto() {
+        val url = auth.currentUser?.photoUrl
         url?.let {
             Glide.with(binding.profileImage.context)
                 .load(url)
@@ -55,24 +95,6 @@ class UserFragment : Fragment() {
                 )
                 .into(binding.profileImage)
         }
-
-
-        //Sign out onclick listener
-        binding.signoutBtn.setOnClickListener {
-            auth.signOut()
-            GoogleSignIn.getClient(requireContext(),
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build())
-                .signOut().addOnCompleteListener{task->
-                    if(task.isSuccessful){
-                        findNavController().navigate(UserFragmentDirections.actionUserFragmentToLoginFragment())
-                    }
-                    else{
-                        Snackbar.make(requireView(),getString(R.string.error_text), Snackbar.LENGTH_SHORT).show()
-                    }
-                }
-        }
-
-        return binding.root
     }
 
 }
